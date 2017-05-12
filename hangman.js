7a4962f74bcbb670da66d13fbce9e469b6359d9f
rolita1007 <rolita1007@gmail.com>
2017-05-12 12:01:01 -0400

    fixed upper case

--- hangman.js
+++ hangman.js
@@ -1,17 +1,53 @@
 $(document).ready(function(){
-
-	var words = ["Hawaii", "Puerto Rico"];
+	var words = ["HAWAII", "PUERTO RICO"];
+	var letter;
 	var selectedWord = words[Math.floor(Math.random() * words.length)];
 	console.log(selectedWord);
 	
 	var answerKey = [];
 	for (var i = 0; i < selectedWord.length; i++) {
 		answerKey[i] = "_";
-		 //need to use .innerHTML to render answerkey on html pg
-		//var remaningLetters = selectedWord.length;
 	}
 	console.log(answerKey.join(" "));
 	$('#question').html(answerKey.join(" "));
 
+	$('#gameBox').on('click', function (event){
+		letter = event.target.innerHTML;
+		console.log(letter);
 
+		for (var i = 0; i < selectedWord.length; i++) {
+			if(selectedWord[i] === letter){
+				answerKey[i] = letter;
+				console.log(answerKey.join(" "));
+			}	
+		}
+
+		$('#question').html(answerKey.join(" "));
+	})
+	
+	
 });
+
+
+
+
+//var remaningLetters = selectedWord.length;
+
+// 	while(remaningLetters > 0) {
+// 		alert(answerKey.join (""));
+
+// 	var guess = prompt ("Guess a letter") 
+// 		if (guess.length !==1) {
+// 			alert("Please enter a letter");
+// 		}else{
+// 			for (var j = 0; j < words.length; j++){
+// 				if(words[j] === guess) {
+// 					answerKey[j] = guess;
+// 					remaningLetters--;  
+// 				}
+// 			}
+// 		}
+	
+// }
+// 	alert(answerKey.join(""));
+// 	alert("Good job!")
